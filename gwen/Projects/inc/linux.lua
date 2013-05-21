5d11e942035e9db23a79849a84dce81a062ed2c2
xelivous <xelivous@pixellegacy.com>
2013-05-20 20:08:21 -0500

    SFML static premake fix

--- gwen/Projects/inc/linux.lua
+++ gwen/Projects/inc/linux.lua
@@ -5,8 +5,10 @@ ALLEGRO_LIBS_D	= { "UnitTest", "Renderer-Allegro", "GWEN-Static", "alleg" }
 
 SFML_LIBS		= { "UnitTest", "Renderer-SFML", "GWEN-Static", "sfml-graphics", "sfml-window", "sfml-system", "GL" }
 SFML_LIBS_D		= { "UnitTest", "Renderer-SFML", "GWEN-Static", "sfml-graphics", "sfml-window", "sfml-system", "GL" }
+SFML_DEFINES	= {}
 
 SFML2_LIBS		= { "UnitTest", "Renderer-SFML2", "GWEN-Static", "sfml-graphics", "sfml-window", "sfml-system", "GL" }
 SFML2_LIBS_D	= { "UnitTest", "Renderer-SFML2", "GWEN-Static", "sfml-graphics", "sfml-window", "sfml-system", "GL" }
+SFML2_DEFINES	= {}
 
 CROSS_LIBS 		= { "UnitTest", "Renderer-OpenGL_DebugFont", "GWEN-Static", "GL", "freeimage" }
